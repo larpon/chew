275a68d13ad90ebaa32d7872ef4c54d8012351b9
lmp <dev.larpon@gmail.com>
2022-08-24 14:48:40 +0200

    main: add license header

--- main.v
+++ main.v
@@ -1,3 +1,6 @@
+// Copyright(C) 2022 Lars Pontoppidan. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
 module main
 
 import os
